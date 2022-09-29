84016bfe80a0263ed1bc363d85cc9e5357b271fb
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-29 12:50:48 +0500

    bring id and classes from DOM

---
+++ custom.js
@@ -0,0 +1,4 @@
+const toggles = document.querySelectorAll('.toggle')
+const good = document.querySelectorAll('#good')
+const cheap = document.querySelectorAll('#cheap')
+const fast = document.querySelectorAll('#fast')
