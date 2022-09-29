6f2173929ee5e34145148bf60a80bb3cdeb97470
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-29 12:59:37 +0500

    arrow function of add event listener

--- custom.js
+++ custom.js
@@ -2,3 +2,5 @@ const toggles = document.querySelectorAll('.toggle')
 const good = document.querySelectorAll('#good')
 const cheap = document.querySelectorAll('#cheap')
 const fast = document.querySelectorAll('#fast')
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
